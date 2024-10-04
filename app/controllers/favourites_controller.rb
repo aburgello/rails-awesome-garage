f3834d590aa033df96200ef97f4cdf20004215da
Antonio Burgello <burgello.antonio@gmail.com>
2024-10-04 11:57:48 +0100

    AJAX

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -2,12 +2,14 @@ class FavouritesController < ApplicationController
   before_action :find_car, only: [ :create, :destroy ]
 
   def create
+    Rails.logger.debug("Parameters: #{params.inspect}")
+
     @favourite = Favourite.new(car: @car)
 
     if @favourite.save
       respond_to do |format|
+        format.js # Keep this if you want to show alerts on the index page via AJAX
         format.html { redirect_to @car, notice: "Favourite was successfully added." }
-        format.js # This will render create.js.erb
       end
     else
       respond_to do |format|
@@ -16,23 +18,29 @@ class FavouritesController < ApplicationController
       end
     end
   end
+
+
   def destroy
+    Rails.logger.debug("Parameters: #{params.inspect}")
+
     @favourite = Favourite.find(params[:id])
     @car = @favourite.car
     @favourite.destroy
 
     respond_to do |format|
-      format.js   # This will render destroy.js.erb
+      format.html { redirect_to @car, notice: "Favourite was successfully removed." }
+      format.js
     end
   end
+
 
   def index
     @favourites = Favourite.all
   end
-end
 
-private
+  private
 
-def find_car
-  @car = Car.find(params[:car_id])
+  def find_car
+    @car = Car.find(params[:car_id])
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,25 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
+import "@rails/ujs";
 
 
 function handleFormSubmission() {
-    // Only target forms with the class 'favourites-form'
-    document.querySelectorAll('form.favourites-form').forEach(form => {
+    document.querySelectorAll('form').forEach(form => {
       form.addEventListener('submit', function(event) {
         event.preventDefault();
-  
         fetch(this.action, {
-          method: form.getAttribute('method') || 'POST',
+            method: 'POST',
+            headers: {
+              'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+              'Accept': 'application/javascript',
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams(new FormData(this))
+          })
+          
+        fetch(this.action, {
+          method: 'POST',
           headers: {
             'Accept': 'application/javascript',
             'Content-Type': 'application/x-www-form-urlencoded',
@@ -29,12 +38,61 @@ function handleFormSubmission() {
     });
   }
   
+function handleDeleteReview() {
+  document.querySelectorAll('.delete-review').forEach(button => {
+    button.addEventListener('click', function(event) {
+      event.preventDefault();
 
+      const reviewId = this.getAttribute('data-review-id');
+      const reviewUrl = this.getAttribute('data-review-url');
 
+      Swal.fire({
+        title: 'Are you sure?',
+        text: 'Once deleted, you will not be able to recover this review!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, keep it'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          fetch(reviewUrl, {
+            method: 'DELETE',
+            headers: {
+              'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+              'Accept': 'application/javascript'
+            }
+          })
+          .then(response => {
+            if (response.ok) {
+              const reviewElement = document.getElementById(`review_${reviewId}`);
+              if (reviewElement) {
+                reviewElement.remove();
+              }
+              Swal.fire({
+                title: 'Deleted!',
+                text: 'Your review has been deleted.',
+                icon: 'success',
+                confirmButtonText: 'OK'
+              });
+            } else {
+              throw new Error('Network response was not ok');
+            }
+          })
+          .catch(error => {
+            console.error('There was a problem with your fetch operation:', error);
+          });
+        } else {
+          Swal.fire('Cancelled', 'Your review is safe :)', 'error');
+        }
+      });
+    });
+  });
+}
 
 document.addEventListener("turbo:load", function () {
   console.log("application.js is loaded");
   handleFormSubmission();
+  handleDeleteReview();
 });
 
 const flashAlert = document.getElementById("flash-alert");
